087b1f8d6f8d0051cff7f6ff46c46b72c538a1ee
Cam McLeod <cammcleod3@gmail.com>
2019-05-19 13:54:20 -0700

    completed tasks encapsulate algorithm in an object and log out the results

---
+++ Largest Number/ArrayUtility.h
@@ -0,0 +1,19 @@
+//
+//  ArrayUtility.h
+//  Largest Number
+//
+//  Created by Cameron Mcleod on 2019-05-19.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArrayUtility : NSObject
+
++(NSNumber) *largestNumber: (NSArray) *array;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Largest Number/ArrayUtility.m
@@ -0,0 +1,17 @@
+//
+//  ArrayUtility.m
+//  Largest Number
+//
+//  Created by Cameron Mcleod on 2019-05-19.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import "ArrayUtility.h"
+
+@implementation ArrayUtility
+
++(NSNumber) *largestNumber: (NSArray) *array {
+    
+}
+
+@end
